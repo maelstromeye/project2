2789e7cd5993cbd187cc3d7bfbe0c09338d7d1b1
maelstromeye <kacperm98@gmail.com>
2018-05-04 17:40:25 +0200

    klasa dziala

--- main.cpp
+++ main.cpp
@@ -2,49 +2,67 @@
 #include <cctype>
 #include <cstdio>
 #include <iostream>
+#include <string>
 using namespace std;
 template <typename type>
 istream &operator>>(istream &input, type &matrix)   //wczytanie z cin
 {
     char space;
-    int crdy;
-    crdy=0;
+    int crdy, flag, read;
+    crdy=read=flag=0;
     do
     {
         do
         {
+            if((flag!=0)&&(read==flag))
+            {
+                cout<<"cut"<<endl;
+                fseek(stdin, 0, SEEK_END);
+                break;
+            }
             input>>matrix.save;
             (matrix[crdy]).push_back(matrix.save);
+            read++;
         }
         while(input.get()!='\n');	//jezeli enter, przechodzimy do nizszej kolumny
+        while ((read!=flag)&&(flag!=0))
+        {
+            matrix.save=0;
+            (matrix[crdy]).push_back(matrix.save);
+            read++;
+        }
         crdy++;
         matrix.init();
-        cout<<matrix.size()<<endl;
+        flag=read;
+        read=0;
     }
     while(input.peek()!='\n');
     return input;
 }
 
 template <typename type>
-ostream &operator<<(ostream &output, type &matrix)   //wypisanie do cout
+ostream &operator<<(ostream &output, Matrix<type> &matrix)   //wypisanie do cout
 {
     int i,j;
-    for(j=0;j+1<matrix.size();j++)
+    for(j=0;j<matrix.size()-1;j++)
     {
         for(i=1;i<(matrix[0]).size();i++)
         {
-            cout<<matrix[j][i]<<' ';
+            output<<matrix[j][i]<<' ';
         }
-        cout<<endl;
+        output<<endl;
     }
     return output;
 }
 
 int main()
 {
-    Matrix<int> mat1;
+    Matrix<int> mat1, mat2;
     cin>>mat1;
+    cout<<mat1<<endl;
+    cin>>mat2;
+    cout<<mat2<<endl;
+    mat1*=mat2;
     cout<<mat1;
-
     return 0;
 }
--- matrix.h
+++ matrix.h
@@ -5,15 +5,161 @@
 template <typename type>
 std::istream &operator>>(std::istream &input, type &matrix);   //wczytanie z cin
 template <typename type>
-std::ostream &operator<<(std::ostream &output, type &matrix);
-template <typename type>
 class Matrix : public std::vector<std::vector<type> >
 {
     private:
         type save;
     public:
         friend std::istream &operator>> <>(std::istream &input, Matrix<type> &matrix);   //wczytanie z cin
-		friend std::ostream &operator<< <>(std::ostream &output, Matrix<type> &matrix);   //wypisanie do cout
+
+		template <typename T>
+		friend std::ostream &operator<< (std::ostream &output, Matrix<T> &matrix);   //wypisanie do cout
+
+        template <typename T>
+        Matrix<T> operator+ (Matrix<T> &matrix)
+        {
+            Matrix<T> blank;
+            if((this->size()!=matrix.size())||(this->at(0).size()!=matrix[0].size())) return blank;
+            int j,i;
+            for(j=0;j<this->size()-1;j++)
+            {
+                for(i=1;i<this->at(0).size();i++)
+                {
+                    blank.save=this->at(j).at(i)+matrix[j][i];
+                    (blank[j]).push_back(blank.save);
+                }
+                blank.init();
+            }
+            return blank;
+        }
+
+        template <typename T>
+        Matrix<T> operator* (Matrix<T> &matrix)
+        {
+            Matrix<T> blank;
+            if((this->at(0).size()!=matrix.size())) return blank;
+            int j,i,n;
+            T sum;
+            for(j=0;j<this->size()-1;j++)
+            {
+                for(i=1;i<matrix.at(0).size();i++)
+                {
+                    for(n=1, sum=0;n<this->at(0).size();n++)
+                    {
+                        sum+=matrix[n-1][i]*this->at(j).at(n);
+                    }
+                    blank.save=sum;
+                    (blank[j]).push_back(blank.save);
+                }
+                blank.init();
+            }
+            return blank;
+        }
+
+        template <typename T>
+        Matrix<T> operator*= (Matrix<T> &matrix)
+        {
+            Matrix<T> blank;
+            if((this->at(0).size()!=matrix.size())) return blank;
+            int j,i,n;
+            T sum;
+            for(j=0;j<this->size()-1;j++)
+            {
+                for(i=1;i<matrix.at(0).size();i++)
+                {
+                    for(n=1, sum=0;n<this->at(0).size();n++)
+                    {
+                        sum+=matrix[n-1][i]*this->at(j).at(n);
+                    }
+                    blank.save=sum;
+                    (blank[j]).push_back(blank.save);
+                }
+                blank.init();
+            }
+            *this=blank;
+            return *this;
+        }
+
+        template <typename T>
+        Matrix<T> operator- (Matrix<T> &matrix)
+        {
+            Matrix<T> blank;
+            if((this->size()!=matrix.size())||(this->at(0).size()!=matrix[0].size())) return blank;
+            int j,i;
+            for(j=0;j<this->size()-1;j++)
+            {
+                for(i=1;i<this->at(0).size();i++)
+                {
+                    blank.save=this->at(j).at(i)-matrix[j][i];
+                    (blank[j]).push_back(blank.save);
+                }
+                blank.init();
+            }
+            return blank;
+        }
+
+        template <typename T>
+        Matrix<T> operator+= (Matrix<T> &matrix)
+        {
+            Matrix<T> blank;
+            if((this->size()!=matrix.size())||(this->at(0).size()!=matrix[0].size())) return blank;
+            int j,i;
+            for(j=0;j<this->size()-1;j++)
+            {
+                for(i=1;i<this->at(0).size();i++)
+                {
+                    this->at(j).at(i)+=matrix[j][i];
+                }
+            }
+            return *this;
+        }
+
+        template <typename T>
+        Matrix<T> operator-= (Matrix<T> &matrix)
+        {
+            Matrix<T> blank;
+            if((this->size()!=matrix.size())||(this->at(0).size()!=matrix[0].size())) return blank;
+            int j,i;
+            for(j=0;j<this->size()-1;j++)
+            {
+                for(i=1;i<this->at(0).size();i++)
+                {
+                    this->at(j).at(i)-=matrix[j][i];
+                }
+            }
+            return blank;
+        }
+
+        template <typename T>
+        bool operator== (Matrix<T> &matrix)
+        {
+            if((this->size()!=matrix.size())||(this->at(0).size()!=matrix[0].size())) return false;
+            int j,i;
+            for(j=0;j<this->size()-1;j++)
+            {
+                for(i=1;i<this->at(0).size();i++)
+                {
+                    if (this->at(j).at(i)!=matrix[j][i]) return false;
+                }
+            }
+            return true;
+        }
+
+        template <typename T>
+        bool operator!= (Matrix<T> &matrix)
+        {
+            if((this->size()!=matrix.size())||(this->at(0).size()!=matrix[0].size())) return true;
+            int j,i;
+            for(j=0;j<this->size()-1;j++)
+            {
+                for(i=1;i<this->at(0).size();i++)
+                {
+                    if (this->at(j).at(i)!=matrix[j][i]) return true;
+                }
+            }
+            return false;
+        }
+
         Matrix(void)
         {
             std::vector<type> temp;
@@ -21,6 +167,17 @@ class Matrix : public std::vector<std::vector<type> >
             this->push_back(temp);
             return;
         }
+
+        ~Matrix(void)
+        {
+            int i;
+            for(i=0;i<this->size();i++)
+            {
+                this->at(i).~vector();
+            }
+            this->clear();
+        }
+
         void init(void)
         {
             std::vector<type> temp;
@@ -28,6 +185,25 @@ class Matrix : public std::vector<std::vector<type> >
             this->push_back(temp);
             return;
         }
+
+        type reveal(int x, int y)
+        {
+            x++;
+            if (x+1>((this->at(0)).size())) return 0;
+            if (y+1>(this->size())-1) return 0;
+            return (this->at(y)).at(x);
+        }
+
+        bool change(int x, int y, type numb)
+        {
+            x++;
+            if (x+1>((this->at(0)).size())) return false;
+            if (y+1>(this->size())-1) return false;
+            (this->at(y)).at(x)=numb;
+            return true;
+        }
 };
 
+
 #endif
+

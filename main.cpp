2715eccf6b8449c733f678d78f6377c08145a9ae
maelstromeye <kacperm98@gmail.com>
2018-05-05 23:52:48 +0200

    Fertig?

--- main.cpp
+++ main.cpp
@@ -1,68 +1,119 @@
 #include "matrix.h"
-#include <cctype>
-#include <cstdio>
-#include <iostream>
 #include <string>
+#include <cstdlib>
+#include <ctime>
 using namespace std;
 template <typename type>
-istream &operator>>(istream &input, type &matrix)   //wczytanie z cin
-{
-    char space;
-    int crdy, flag, read;
-    crdy=read=flag=0;
-    do
-    {
-        do
-        {
-            if((flag!=0)&&(read==flag))
-            {
-                cout<<"cut"<<endl;
-                fseek(stdin, 0, SEEK_END);
-                break;
-            }
-            input>>matrix.save;
-            (matrix[crdy]).push_back(matrix.save);
-            read++;
-        }
-        while(input.get()!='\n');	//jezeli enter, przechodzimy do nizszej kolumny
-        while ((read!=flag)&&(flag!=0))
-        {
-            matrix.save=0;
-            (matrix[crdy]).push_back(matrix.save);
-            read++;
-        }
-        crdy++;
-        matrix.init();
-        flag=read;
-        read=0;
-    }
-    while(input.peek()!='\n');
-    return input;
-}
-
+bool general_test(void);
 template <typename type>
-ostream &operator<<(ostream &output, Matrix<type> &matrix)   //wypisanie do cout
-{
-    int i,j;
-    for(j=0;j<matrix.size()-1;j++)
-    {
-        for(i=1;i<(matrix[0]).size();i++)
-        {
-            output<<matrix[j][i]<<' ';
-        }
-        output<<endl;
-    }
-    return output;
-}
-
+void adapt(Matrix<type> &matrix, int y, int x);
+int change(void);
+int next(int x);
 int main()
 {
-    Matrix<int> mat1, mat2;
-    cin>>mat1;
-    cout<<mat1<<endl;
-    cin>>mat2;
-    cout<<mat2<<endl;
-    mat1*=mat2;
-    cout<<mat1;
+    string command;
+    while(1)
+    {
+        cout<<"Welcome! Choose what you want to do."<<endl;
+        cout<<"-'test' to run a general test of random matrices of specified type"<<endl;
+        cout<<"-'kill' to end"<<endl;
+        cin>>command;
+        if (command=="test")
+        {
+            switch(change())
+            {
+                case 1:
+                    if(general_test<double>()) cout<<"test pass"<<endl;
+                    else cout<<"test fail"<<endl;
+                    break;
+                case 2:
+                    if(general_test<complex<int> >()) cout<<"test pass"<<endl;
+                    else cout<<"test fail"<<endl;
+                    break;
+                case 3:
+                    if(general_test<int>()) cout<<"test pass"<<endl;
+                    else cout<<"test fail"<<endl;
+                    break;
+                case 4:
+                    if(general_test<complex<double> >()) cout<<"test pass"<<endl;
+                    else cout<<"test fail"<<endl;
+                    break;
+                default:
+                    break;
+            }
+        }
+        else if(command=="kill") return 0;
+        else cout<<"Unknown command"<<endl;
+    }
     return 0;
 }
+template <typename type>
+bool general_test(void)
+{
+    int i,j;
+    Matrix<type> matrices[4];
+    type n;
+    cout<<"Construction...ok"<<endl;
+    cout<<"Please input a matrix"<<endl;
+    cin>>matrices[0];
+    cout<<"Input...ok"<<endl;
+    cout<<matrices[0]<<endl<<"Output...ok"<<endl<<"Randomizing matrix 2 with "<<matrices[0].size()-1<<"x"<<matrices[0][0].size()-1<<" dimensions"<<endl;
+    matrices[1].adapt(matrices[0].size()-1, matrices[0][0].size()-1);
+    cout<<matrices[1];
+    cout<<"Comparing 2 matrices"<<endl;
+    if (matrices[0]!=matrices[0]) {cout<<"Comparison fail... Aborting..."<<endl; return false;}
+    if (!(matrices[0]==matrices[0])) {cout<<"Comparison fail... Aborting..."<<endl; return false;}
+    if (matrices[0]==matrices[1]) cout<<"Matrices equal, comparison ok"<<endl;
+    else cout<<"Matrices not equal, comparison ok"<<endl;
+    cout<<"Adding both matrices"<<endl;
+    matrices[2]=matrices[0]+matrices[1];
+    cout<<matrices[2];
+    cout<<"Adding with assignment"<<endl;
+    matrices[0]+=matrices[1];
+    cout<<matrices[0];
+    if (matrices[0]==matrices[2]) cout<<"Simple maths...ok"<<endl;
+    else {cout<<"Simple maths fail... Aborting..."<<endl; return false;}
+    cout<<"Generating random matrix multipliable by the one above..."<<endl;
+    matrices[3].adapt(matrices[0][0].size()-1, matrices[0].size()-1);
+    cout<<matrices[3];
+    cout<<"Multiplying matrices..."<<endl;
+    matrices[2]=matrices[0]*matrices[3];
+    cout<<matrices[2];
+    cout<<"Multiplying with assignment..."<<endl;
+    matrices[0]*=matrices[3];
+    cout<<matrices[0];
+    if (matrices[0]==matrices[2]) cout<<"Multiplication...ok"<<endl;
+    else {cout<<"Multiplication fail... Aborting..."<<endl; return false;}
+    cout<<"Testing accessing and changing elements... Access element of matrix 1:"<<endl;
+    cout<<matrices[0];
+    i=rand()%(matrices[0][0].size()-1);
+    j=rand()%(matrices[0].size()-1);
+    cout<<"At:("<<j<<","<<i<<")"<<endl<<matrices[0].reveal(i,j)<<endl;
+    if(matrices[0].reveal(i,j)!=matrices[0].at(j).at(i+1)) {cout<<"Access fail... Aborting..."<<endl; return false;}
+    else cout<<"Access...ok"<<endl;
+    n=rand()/rand();
+    cout<<"Changing element at ("<<j<<","<<i<<") of matrix 1 to: "<<n<<endl;
+    matrices[0].change(i,j,n);
+    cout<<"Matrix 1 is now:"<<endl;
+    cout<<matrices[0];
+    if (matrices[0].reveal(i,j)!=n) {cout<<"Element change fail... Aborting..."<<endl; return false;}
+    else cout<<"Element change...ok"<<endl;
+    cout<<"All matrix functions operational"<<endl;
+    return true;
+}
+int change(void)
+{
+    string command;
+    cout<<"Specify the type (int, double, complexint, complexdouble)"<<endl;
+    cin>>command;
+    if (command=="double") return 1;
+    else if (command=="complexint") return 2;
+    else if (command=="int") return 3;
+    else if (command=="complexdouble") return 4;
+    else
+    {
+        cout<<"Unknown type, assuming int"<<endl;
+    }
+    return 3;
+}
+
--- matrix.h
+++ matrix.h
@@ -1,63 +1,81 @@
 #ifndef DEF
 #define DEF
-#include<iostream>
-#include<vector>
+#include <iostream>
+#include <cstdio>
+#include <vector>
+#include <complex>
+#include <cstdlib>
+#include <ctime>
 template <typename type>
-std::istream &operator>>(std::istream &input, type &matrix);   //wczytanie z cin
-template <typename type>
-class Matrix : public std::vector<std::vector<type> >
+class Matrix : public std::vector<std::vector<type> >   //dziedziczenie z std::vector
 {
     private:
-        type save;
+        type save;  //potrzebne do zapisywania
     public:
-        friend std::istream &operator>> <>(std::istream &input, Matrix<type> &matrix);   //wczytanie z cin
+        template <typename T>
+        friend std::istream &operator>> (std::istream &input, Matrix<T> &matrix);   //wczytanie z cin
 
 		template <typename T>
 		friend std::ostream &operator<< (std::ostream &output, Matrix<T> &matrix);   //wypisanie do cout
 
-        template <typename T>
-        Matrix<T> operator+ (Matrix<T> &matrix)
+        void adapt(int y, int x)
         {
-            Matrix<T> blank;
-            if((this->size()!=matrix.size())||(this->at(0).size()!=matrix[0].size())) return blank;
-            int j,i;
-            for(j=0;j<this->size()-1;j++)
+            int i,j;
+            srand(time(NULL));
+            for(j=0;j<y;j++)
             {
-                for(i=1;i<this->at(0).size();i++)
+                for(i=0;i<x;i++)
+                {
+                    this->save=rand()/rand();
+                    this->at(j).push_back(this->save);
+                }
+                this->init();
+            }
+        }
+
+        template <typename T>
+        Matrix<T> operator+ (Matrix<T> &matrix) //dodawanie
+        {
+            Matrix<T> blank;    //na tym pracujemy
+            if((this->size()!=matrix.size())||(this->at(0).size()!=matrix[0].size())) return blank; //jezeli zle wymiary
+            int j,i;    //countery
+            for(j=0;j<this->size()-1;j++)   //zewnetrzny wektor
+            {
+                for(i=1;i<this->at(0).size();i++)   //wewnetrzny wektor (na pozycji 0 zawsze sa 0)
                 {
                     blank.save=this->at(j).at(i)+matrix[j][i];
-                    (blank[j]).push_back(blank.save);
+                    (blank[j]).push_back(blank.save);   //zapisanie
                 }
-                blank.init();
+                blank.init();   //nowy wiersz
             }
             return blank;
         }
 
         template <typename T>
-        Matrix<T> operator* (Matrix<T> &matrix)
+        Matrix<T> operator* (Matrix<T> &matrix) //mnozenie
         {
-            Matrix<T> blank;
-            if((this->at(0).size()!=matrix.size())) return blank;
-            int j,i,n;
-            T sum;
-            for(j=0;j<this->size()-1;j++)
+            Matrix<T> blank;    //na tym pracujemy
+            if((this->at(0).size()!=matrix.size())) return blank;   //zle wymiary
+            int j,i,n;  //countery
+            T sum;  //suma mnozenia wektor*wektor
+            for(j=0;j<this->size()-1;j++)   //zewmnetrzny wektor
             {
-                for(i=1;i<matrix.at(0).size();i++)
+                for(i=1;i<matrix.at(0).size();i++)  //wewnetrzny wektor
                 {
-                    for(n=1, sum=0;n<this->at(0).size();n++)
+                    for(n=1, sum=0;n<this->at(0).size();n++)    //mnozenie
                     {
-                        sum+=matrix[n-1][i]*this->at(j).at(n);
+                        sum+=matrix[n-1][i]*this->at(j).at(n);  //to zapiszemy
                     }
                     blank.save=sum;
-                    (blank[j]).push_back(blank.save);
+                    (blank[j]).push_back(blank.save);   //zapisujemy
                 }
-                blank.init();
+                blank.init();   //nowy wiersz
             }
             return blank;
         }
 
         template <typename T>
-        Matrix<T> operator*= (Matrix<T> &matrix)
+        Matrix<T> operator*= (Matrix<T> &matrix)    //analogicznie do *
         {
             Matrix<T> blank;
             if((this->at(0).size()!=matrix.size())) return blank;
@@ -81,7 +99,7 @@ class Matrix : public std::vector<std::vector<type> >
         }
 
         template <typename T>
-        Matrix<T> operator- (Matrix<T> &matrix)
+        Matrix<T> operator- (Matrix<T> &matrix) //analogicznie do +
         {
             Matrix<T> blank;
             if((this->size()!=matrix.size())||(this->at(0).size()!=matrix[0].size())) return blank;
@@ -99,7 +117,7 @@ class Matrix : public std::vector<std::vector<type> >
         }
 
         template <typename T>
-        Matrix<T> operator+= (Matrix<T> &matrix)
+        Matrix<T> operator+= (Matrix<T> &matrix)    //analogicznie do +
         {
             Matrix<T> blank;
             if((this->size()!=matrix.size())||(this->at(0).size()!=matrix[0].size())) return blank;
@@ -115,7 +133,7 @@ class Matrix : public std::vector<std::vector<type> >
         }
 
         template <typename T>
-        Matrix<T> operator-= (Matrix<T> &matrix)
+        Matrix<T> operator-= (Matrix<T> &matrix) //analogicznie do -
         {
             Matrix<T> blank;
             if((this->size()!=matrix.size())||(this->at(0).size()!=matrix[0].size())) return blank;
@@ -131,7 +149,7 @@ class Matrix : public std::vector<std::vector<type> >
         }
 
         template <typename T>
-        bool operator== (Matrix<T> &matrix)
+        bool operator== (Matrix<T> &matrix) //analogicznie do +, tylko sprawdzenie rownosci zamiast dzialania i zwracanie false/true
         {
             if((this->size()!=matrix.size())||(this->at(0).size()!=matrix[0].size())) return false;
             int j,i;
@@ -146,7 +164,7 @@ class Matrix : public std::vector<std::vector<type> >
         }
 
         template <typename T>
-        bool operator!= (Matrix<T> &matrix)
+        bool operator!= (Matrix<T> &matrix) //analogicznie do ==
         {
             if((this->size()!=matrix.size())||(this->at(0).size()!=matrix[0].size())) return true;
             int j,i;
@@ -160,7 +178,7 @@ class Matrix : public std::vector<std::vector<type> >
             return false;
         }
 
-        Matrix(void)
+        Matrix(void)    //konstruktor
         {
             std::vector<type> temp;
             temp.push_back(0);
@@ -168,17 +186,17 @@ class Matrix : public std::vector<std::vector<type> >
             return;
         }
 
-        ~Matrix(void)
+        ~Matrix(void)   //destruktor
         {
             int i;
-            for(i=0;i<this->size();i++)
+            for(i=0;i<this->size();i++) //niszczymy kazdy wektor wewnetrzny
             {
                 this->at(i).~vector();
             }
-            this->clear();
+            this->clear();  //i zedwnetrzny
         }
 
-        void init(void)
+        void init(void) //utworzenie nowego wiersza (z jakiegos powodu callowanie konstruktora nie dzialalo)
         {
             std::vector<type> temp;
             temp.push_back(0);
@@ -186,15 +204,15 @@ class Matrix : public std::vector<std::vector<type> >
             return;
         }
 
-        type reveal(int x, int y)
+        type reveal(int x, int y)   //odkrycie elementu na danych wspolrzednych
         {
             x++;
-            if (x+1>((this->at(0)).size())) return 0;
+            if (x+1>((this->at(0)).size())) return 0;   //poza zasiegiem
             if (y+1>(this->size())-1) return 0;
             return (this->at(y)).at(x);
         }
 
-        bool change(int x, int y, type numb)
+        bool change(int x, int y, type numb)    //zmiana wartosci elementu, analogicznie do reveal
         {
             x++;
             if (x+1>((this->at(0)).size())) return false;
@@ -203,7 +221,55 @@ class Matrix : public std::vector<std::vector<type> >
             return true;
         }
 };
+using namespace std;
+template <typename type>
+istream &operator>>(istream &input, Matrix<type> &matrix)   //wczytanie z cin
+{
+    int crdy, flag, read;
+    crdy=read=flag=0;
+    do
+    {
+        do
+        {
+            if((flag!=0)&&(read==flag))
+            {
+                cout<<"cut"<<endl;
+                fseek(stdin, 0, SEEK_END);
+                break;
+            }
+            input>>matrix.save;
+            (matrix[crdy]).push_back(matrix.save);
+            read++;
+        }
+        while(input.get()!='\n');	//jezeli enter, przechodzimy do nizszej kolumny
+        while ((read!=flag)&&(flag!=0))
+        {
+            matrix.save=0;
+            (matrix[crdy]).push_back(matrix.save);
+            read++;
+        }
+        crdy++;
+        matrix.init();
+        flag=read;
+        read=0;
+    }
+    while(input.peek()!='\n');
+    return input;
+}
 
-
+template <typename type>
+ostream &operator<<(ostream &output, Matrix<type> &matrix)   //wypisanie do cout
+{
+    int i,j;
+    for(j=0;j<matrix.size()-1;j++)
+    {
+        for(i=1;i<(matrix[0]).size();i++)
+        {
+            output<<matrix[j][i]<<' ';
+        }
+        output<<endl;
+    }
+    return output;
+}
 #endif
 
